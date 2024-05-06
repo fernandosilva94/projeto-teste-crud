06a4e0c6a720462e7fb7f30376b99cbc649fe8c6
theonnogueira <theonnogueira@outlook.com>
2024-05-06 18:20:34 -0300

    Ajustes no componente login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(private router: Router) { }
   ngOnInit(): void {
+
+  }
+  back() {
+    this.router.navigate(['/dashboard']);
 
   }
 
